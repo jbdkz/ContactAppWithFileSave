9884e2b320dca02eddb809e11f332a2bdfd5212f
John Diczhazy <jbdkz@sbcglobal.net>
2018-12-14 23:06:07 -0500

    Initial Commit

---
+++ ContactAppWithFileSave/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ContactAppWithFileSave
+//
+//  Created by John Diczhazy on 12/14/18.
+//  Copyright © 2018 John Diczhazy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
